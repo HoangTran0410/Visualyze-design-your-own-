4c61f882ebb931771866f04ace77ff3a796315b7
Hoang Tran <th01634044850@gmail.com>
2019-05-25 23:51:34 +0700

    hmmm

--- Js/Sketch.js
+++ Js/Sketch.js
@@ -30,266 +30,269 @@ var client_id = '587aa2d384f7333a886010d5f52f302a'; // Soundcloud
 var gui;
 
 var songFromLocation = {
-	haveSong: false,
-	indexSong: 0
+    haveSong: false,
+    indexSong: 0
 }
 
 function setup() {
-	// first setting
-	createCanvas(windowWidth, windowHeight).smooth().position(0, 0).drop(getFileLocal);
-	colorMode(HSB);
-	angleMode(DEGREES);
-	imageMode(CENTER);
-	rectMode(CENTER);
-	textSize(20);
-	textAlign(CENTER, CENTER);
+    // first setting
+    createCanvas(windowWidth, windowHeight).smooth().position(0, 0).drop(getFileLocal);
+    colorMode(HSB);
+    angleMode(DEGREES);
+    imageMode(CENTER);
+    rectMode(CENTER);
+    textSize(20);
+    textAlign(CENTER, CENTER);
 
-	preWidth = width;
-	preHeight = height;
-	mic = new p5.AudioIn();
-	AmpData = new p5.Amplitude();
-	FftData = new p5.FFT(0.4, 1024);
+    preWidth = width;
+    preHeight = height;
+    mic = new p5.AudioIn();
+    AmpData = new p5.Amplitude();
+    FftData = new p5.FFT(0.4, 1024);
 
-	// add object
-	info = new InfoSong();
-	rectChooseMulti = new rectChooseMultiObject();
-	addGui();
+    // add object
+    info = new InfoSong();
+    rectChooseMulti = new rectChooseMultiObject();
+    addGui();
 
-	// from href
-	var l = window.location.href;
-	if (l.search("[?]") > 0) {
-		l = l.substring(l.search("[?]") + 1);
-		var check = l.substring(0, l.search("[=]"));
-		if (check == 'customtheme') {
-			l = l.substring(l.search("[=]") + 1)
-				.replace(/\%20|\+/gi, " ")
-				.replace(/\%22/gi, "\"")
-				.replace(/\%7B/gi, "{")
-				.replace(/\%7D/gi, "{")
-				.replace(/\%5B/gi, "[")
-				.replace(/\%5D/gi, "]");
+    // from href
+    var l = window.location.href;
+    if (l.search("[?]") > 0) {
+        l = l.substring(l.search("[?]") + 1);
+        var check = l.substring(0, l.search("[=]"));
+        if (check == 'customtheme') {
+            l = l.substring(l.search("[=]") + 1)
+                .replace(/\%20|\+/gi, " ")
+                .replace(/\%22/gi, "\"")
+                .replace(/\%7B/gi, "{")
+                .replace(/\%7D/gi, "{")
+                .replace(/\%5B/gi, "[")
+                .replace(/\%5D/gi, "]");
 
-			var data = JSON.parse(l);
-			loadTheme(data, false, true, true);
+            var data = JSON.parse(l);
+            loadTheme(data, false, true, true);
 
-			// VisualizeGui.playlists = PlayList[floor(random(PlayList.length))].name;
-			// getPlaylist(VisualizeGui.playlists);
-		} else
-			while (l.search("[=]") > 0) {
-				var sch = l.search("[&]");
-				var left = l.substring(0, l.search("[=]"));
-				var right = l.substring(l.search("[=]") + 1, (sch > 0) ? sch : l.length);
-				if (sch > 0) l = l.substring(sch + 1);
-				else l = "";
+            // VisualizeGui.playlists = PlayList[floor(random(PlayList.length))].name;
+            // getPlaylist(VisualizeGui.playlists);
+        } else
+            while (l.search("[=]") > 0) {
+                var sch = l.search("[&]");
+                var left = l.substring(0, l.search("[=]"));
+                var right = l.substring(l.search("[=]") + 1, (sch > 0) ? sch : l.length);
+                if (sch > 0) l = l.substring(sch + 1);
+                else l = "";
 
-				console.log(left + '=' + right);
+                console.log(left + '=' + right);
 
-				switch (left) {
-					case "theme":
-						VisualizeGui.themes = right;
-						loadJSON('default theme/' + right + '.json',
-							// loaded
-							function(data) {
-								loadTheme(data, false);
-							}
-						);
-						break;
-					case "playlist":
-						getPlaylist(PlayList[right - 1].name, 'notPlay');
-						break;
-					case "song":
-						songFromLocation.haveSong = true;
-						songFromLocation.indexSong = right - 1;
-						break;
-					case "linksong":
-						songFromLocation.haveSong = true;
-						songFromLocation.indexSong = right;
-						break;
-					case "background":
-						backgNow = right - 1;
-						VisualizeGui.backgs = BackList[backgNow].name;
-						backG = loadImage(BackList[backgNow].link);
-						break;
-				}
-			}
-	} else {
-		// background
-		backgNow = floor(random(0, BackList.length));
-		VisualizeGui.backgs = BackList[backgNow].name;
-		backG = loadImage(BackList[backgNow].link);
+                switch (left) {
+                    case "theme":
+                        VisualizeGui.themes = right;
+                        loadJSON('default theme/' + right + '.json',
+                            // loaded
+                            function(data) {
+                                loadTheme(data, false);
+                            }
+                        );
+                        break;
+                    case "playlist":
+                        getPlaylist(PlayList[right - 1].name, 'notPlay');
+                        break;
+                    case "song":
+                        songFromLocation.haveSong = true;
+                        songFromLocation.indexSong = right - 1;
+                        break;
+                    case "linksong":
+                        songFromLocation.haveSong = true;
+                        songFromLocation.indexSong = right;
+                        break;
+                    case "background":
+                        backgNow = right - 1;
+                        VisualizeGui.backgs = BackList[backgNow].name;
+                        backG = loadImage(BackList[backgNow].link);
+                        break;
+                }
+            }
+    } else {
+        // background
+        backgNow = floor(random(0, BackList.length));
+        VisualizeGui.backgs = BackList[backgNow].name;
+        backG = loadImage(BackList[backgNow].link);
 
-		// create Audio random
-		while(VisualizeGui.playlists == "" || VisualizeGui.playlists.indexOf("=") >= 0) {
-			// VisualizeGui.playlists = PlayList[8].name;
-			VisualizeGui.playlists = PlayList[floor(random(PlayList.length))].name;
-		}
-		getPlaylist(VisualizeGui.playlists);
-		showFolder('Audio');
+        // create Audio random
+        while (VisualizeGui.playlists == "" || VisualizeGui.playlists.indexOf("=") >= 0) {
+            // VisualizeGui.playlists = PlayList[8].name;
+            VisualizeGui.playlists = PlayList[floor(random(PlayList.length))].name;
+        }
+        getPlaylist(VisualizeGui.playlists);
+        showFolder('Audio');
 
-		// load theme
-		var nameTheme = random(['HauMaster', 'HoangTran', 'HauMasterLite']);
-		VisualizeGui.themes = nameTheme;
-		loadJSON('default theme/' + nameTheme + '.json',
-			// loaded
-			function(data) {
-				loadTheme(data);
-			},
-			// error
-			function(e) {
-				alert("Can not load this theme, choose another theme in Setting\nERROR:" + e);
-				createNewAudio("Theo Anh - Ali Hoang Duong.mp3");
-				info.setTitle('Theo Anh - Ali Hoang Duong.mp3', 'file');
-			}
-		);
-	}
+        // load theme
+        var nameTheme = random(['HauMaster', 'HoangTran', 'HauMasterLite']);
+        VisualizeGui.themes = nameTheme;
+        loadJSON('default theme/' + nameTheme + '.json',
+            // loaded
+            function(data) {
+                loadTheme(data);
+            },
+            // error
+            function(e) {
+                alert("Can not load this theme, choose another theme in Setting\nERROR:" + e);
+                createNewAudio("Theo Anh - Ali Hoang Duong.mp3");
+                info.setTitle('Theo Anh - Ali Hoang Duong.mp3', 'file');
+            }
+        );
+    }
 }
 
 function draw() {
-	if ((focused && VisualizeGui.checkFocus) || !VisualizeGui.checkFocus) {
-		if (songFromLocation.haveSong) {
-			if (isNaN(songFromLocation.indexSong)) {
-				addAudio(songFromLocation.indexSong);
-				songFromLocation.haveSong = false;
+    if ((focused && VisualizeGui.checkFocus) || !VisualizeGui.checkFocus) {
+        if (songFromLocation.haveSong) {
+            if (isNaN(songFromLocation.indexSong)) {
+                addAudio(songFromLocation.indexSong);
+                songFromLocation.haveSong = false;
 
-			} else if (SongList[songFromLocation.indexSong]) {
-				addAudio(SongList[songFromLocation.indexSong].link);
-				songFromLocation.haveSong = false;
-			}
-		}
-		animationBackground();
-		autoChangeBackFunc();
+            } else if (SongList[songFromLocation.indexSong]) {
+                addAudio(SongList[songFromLocation.indexSong].link);
+                songFromLocation.haveSong = false;
+            }
+        }
+        animationBackground();
+        autoChangeBackFunc();
 
-		if (second() - mouseActive > 1 && gui.closed) // auto hide dat.GUI
-			gui.domElement.style.display = "none";
+        if (second() - mouseActive > 1 && gui.closed) // auto hide dat.GUI
+            gui.domElement.style.display = "none";
 
-		// get data to visualyze
-		if (myAudio) {
-			ampLevel = AmpData.getLevel();
-			fftWave = FftData.waveform();
-			fftAnalyze = FftData.analyze();
-			fftAnalyze.splice(65, 1024 - 64);
+        // get data to visualyze
+        if (myAudio) {
+            ampLevel = AmpData.getLevel();
+            fftWave = FftData.waveform();
+            fftAnalyze = FftData.analyze();
+            fftAnalyze.splice(65, 1024 - 64);
 
-			// run all objects
-			for (var i = 0; i < objects.length; i++)
-				objects[i].run();
-		}
+            // run all objects
+            for (var i = 0; i < objects.length; i++)
+                objects[i].run();
+        }
 
-		// choose multi object
-		if (rectChooseMulti.isActive && designMode) {
-			rectChooseMulti.show();
-		}
-	}
+        // choose multi object
+        if (rectChooseMulti.isActive && designMode) {
+            rectChooseMulti.show();
+        }
+    }
 }
 
 function keyPressed() {
-	if (keyCode == 83) { // S key
-		designMode = !designMode;
-		VisualizeGui.showDesignMode = designMode;
-		if (designMode) showFolder('Design');
+    if (keyCode == 83) { // S key
+        designMode = !designMode;
+        VisualizeGui.showDesignMode = designMode;
+        if (designMode) showFolder('Design');
 
-	} else if (keyCode == LEFT_ARROW) {
-		if (myAudio.elt.currentTime >= 5 && !myAudio.elt.paused)
-			myAudio.play().time(myAudio.elt.currentTime - 5);
+    } else if (keyCode == LEFT_ARROW) {
+        if (myAudio.elt.currentTime >= 5 && !myAudio.elt.paused)
+            myAudio.play().time(myAudio.elt.currentTime - 5);
 
-	} else if (keyCode == RIGHT_ARROW && !myAudio.elt.paused) {
-		if (myAudio.elt.currentTime < myAudio.elt.duration - 5)
-			myAudio.play().time(myAudio.elt.currentTime + 5);
-	
-	} else if (keyCode == UP_ARROW && !myAudio.elt.paused) {
-		myAudio.elt.volume += 0.1;
-		if(myAudio.elt.volume > 1)
-			myAudio.elt.volume = 1;
-		VisualizeGui.volume = myAudio.elt.volume;
+    } else if (keyCode == RIGHT_ARROW && !myAudio.elt.paused) {
+        if (myAudio.elt.currentTime < myAudio.elt.duration - 5)
+            myAudio.play().time(myAudio.elt.currentTime + 5);
 
-	} else if (keyCode == DOWN_ARROW && !myAudio.elt.paused) {
-		myAudio.elt.volume -= 0.1;
-		if(myAudio.elt.volume < 0)
-			myAudio.elt.volume = 0;
-		VisualizeGui.volume = myAudio.elt.volume;
+    } else if (keyCode == UP_ARROW && !myAudio.elt.paused) {
+        myAudio.elt.volume += 0.1;
+        if (myAudio.elt.volume > 1)
+            myAudio.elt.volume = 1;
+        VisualizeGui.volume = myAudio.elt.volume;
 
-	} else if (keyCode == 67) { // C key
-		if (myAudio) {
-			if (!myAudio.elt.controls)
-				myAudio.showControls();
-			else myAudio.hideControls();
-		}
-	}
+    } else if (keyCode == DOWN_ARROW && !myAudio.elt.paused) {
+        myAudio.elt.volume -= 0.1;
+        if (myAudio.elt.volume < 0)
+            myAudio.elt.volume = 0;
+        VisualizeGui.volume = myAudio.elt.volume;
+
+    } else if (keyCode == 67) { // C key
+        if (myAudio) {
+            if (!myAudio.elt.controls)
+                myAudio.showControls();
+            else myAudio.hideControls();
+        }
+    }
 }
 
 function mousePressed() {
-	if (designMode) {
-		for (var i = 0; i < objects.length; i++)
-			objects[i].boxcontain.mouseChoose();
+    if (myAudio && getAudioContext().state !== 'running') {
+        getAudioContext().resume();
+    }
+    if (designMode) {
+        for (var i = 0; i < objects.length; i++)
+            objects[i].boxcontain.mouseChoose();
 
-		// if choose multi object => when click mouse 
-		// => need to check position of mouse
-		// => is mouse position inside any 'choosed' object
-		var foundObjectInChoose = false;
-		for (var i = 0; i < objects.length; i++) {
-			if (objects[i].boxcontain.allowChangepos &&
-				objects[i].boxcontain.chooseMulti) {
-				foundObjectInChoose = true;
-				break;
-			}
-		}
+        // if choose multi object => when click mouse 
+        // => need to check position of mouse
+        // => is mouse position inside any 'choosed' object
+        var foundObjectInChoose = false;
+        for (var i = 0; i < objects.length; i++) {
+            if (objects[i].boxcontain.allowChangepos &&
+                objects[i].boxcontain.chooseMulti) {
+                foundObjectInChoose = true;
+                break;
+            }
+        }
 
-		if (!foundObjectInChoose)
-			for (var i = 0; i < objects.length; i++)
-				objects[i].boxcontain.setChooseMulti(false);
+        if (!foundObjectInChoose)
+            for (var i = 0; i < objects.length; i++)
+                objects[i].boxcontain.setChooseMulti(false);
 
-	} else {
-		// check if mouse click on a button
-		for (var i = 0; i < objects.length; i++) {
-			if (objects[i].objectType == 'ButtonShape')
-				objects[i].clicked();
-		}
-	}
+    } else {
+        // check if mouse click on a button
+        for (var i = 0; i < objects.length; i++) {
+            if (objects[i].objectType == 'ButtonShape')
+                objects[i].clicked();
+        }
+    }
 }
 
 function mouseDragged() {
-	if (designMode) {
-		for (var i = 0; i < objects.length; i++)
-			objects[i].boxcontain.drag();
+    if (designMode) {
+        for (var i = 0; i < objects.length; i++)
+            objects[i].boxcontain.drag();
 
-		if (keyIsDown(CONTROL) && rectChooseMulti.isActive) {
-			// save end pos while drag + ctrl
-			rectChooseMulti.setEnd(mouseX, mouseY);
+        if (keyIsDown(CONTROL) && rectChooseMulti.isActive) {
+            // save end pos while drag + ctrl
+            rectChooseMulti.setEnd(mouseX, mouseY);
 
-		} else {
-			rectChooseMulti.setActive(true);
-			// begin and end position will = first pos + ctrl
-			rectChooseMulti.setBegin(mouseX, mouseY);
-			rectChooseMulti.setEnd(mouseX, mouseY);
-		}
-	}
+        } else {
+            rectChooseMulti.setActive(true);
+            // begin and end position will = first pos + ctrl
+            rectChooseMulti.setBegin(mouseX, mouseY);
+            rectChooseMulti.setEnd(mouseX, mouseY);
+        }
+    }
 }
 
 function mouseReleased() {
-	if (rectChooseMulti.isActive && designMode) {
-		for (var i = 0; i < objects.length; i++) {
-			if (isPointInsideRect(objects[i].pos, rectChooseMulti.beginPoint, rectChooseMulti.endPoint))
-				objects[i].boxcontain.setChooseMulti(true);
-		}
-		rectChooseMulti.setActive(false);
-	}
+    if (rectChooseMulti.isActive && designMode) {
+        for (var i = 0; i < objects.length; i++) {
+            if (isPointInsideRect(objects[i].pos, rectChooseMulti.beginPoint, rectChooseMulti.endPoint))
+                objects[i].boxcontain.setChooseMulti(true);
+        }
+        rectChooseMulti.setActive(false);
+    }
 }
 
 function mouseMoved() {
-	mouseActive = second();
-	gui.domElement.style.display = "";
+    mouseActive = second();
+    gui.domElement.style.display = "";
 }
 
 function windowResized() {
-	for (var i = 0; i < objects.length; i++) {
-		var newPos = createVector(objects[i].pos.x / preWidth * windowWidth, objects[i].pos.y / preHeight * windowHeight);
-		var newSize = createVector(objects[i].size.x / preWidth * windowWidth, objects[i].size.y / preHeight * windowHeight);;
-		objects[i].setPosition(newPos.x, newPos.y);
-		objects[i].setSize(newSize.x, newSize.y);
+    for (var i = 0; i < objects.length; i++) {
+        var newPos = createVector(objects[i].pos.x / preWidth * windowWidth, objects[i].pos.y / preHeight * windowHeight);
+        var newSize = createVector(objects[i].size.x / preWidth * windowWidth, objects[i].size.y / preHeight * windowHeight);;
+        objects[i].setPosition(newPos.x, newPos.y);
+        objects[i].setSize(newSize.x, newSize.y);
 
-		objects[i].boxcontain.applyPosition();
-		objects[i].boxcontain.applySize();
-	}
-	resizeCanvas(windowWidth, windowHeight, true);
-	preWidth = windowWidth;
-	preHeight = windowHeight;
+        objects[i].boxcontain.applyPosition();
+        objects[i].boxcontain.applySize();
+    }
+    resizeCanvas(windowWidth, windowHeight, true);
+    preWidth = windowWidth;
+    preHeight = windowHeight;
 }
